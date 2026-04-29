0421b03e0f2a9c240e32bf38b29e52c781851eda
kimngan1008 <phanthikimn88@gmail.com>
2026-04-29 13:11:57 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // 1. Xuất ra web tĩnh
   output: "export",
-
-  // 2. Đường dẫn web (Phải khớp chính xác tên Repository của bạn)
   basePath: "/nhom10_bookstation",
-
-  // 3. Tắt tối ưu ảnh
   images: {
     unoptimized: true,
   },
-
-  // 4. Bỏ qua lỗi để lấy tích xanh
   typescript: {
     ignoreBuildErrors: true, 
   },
